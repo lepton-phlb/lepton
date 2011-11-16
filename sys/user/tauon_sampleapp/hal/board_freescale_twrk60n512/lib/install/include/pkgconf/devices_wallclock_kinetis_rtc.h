b173b782f17a5b8f2b6d3d67445dca99c6a7ace6
lepton.jjp <lepton.jjp@gmail.com>
2011-11-16 15:54:43 +0100

    add HAL for TWR-K60N512

---
+++ sys/user/tauon_sampleapp/hal/board_freescale_twrk60n512/lib/install/include/pkgconf/devices_wallclock_kinetis_rtc.h
@@ -0,0 +1,13 @@
+#ifndef CYGONCE_PKGCONF_DEVICES_WALLCLOCK_KINETIS_RTC_H
+#define CYGONCE_PKGCONF_DEVICES_WALLCLOCK_KINETIS_RTC_H
+/*
+ * File <pkgconf/devices_wallclock_kinetis_rtc.h>
+ *
+ * This file is generated automatically by the configuration
+ * system. It should not be edited. Any changes to this file
+ * may be overwritten.
+ */
+
+#define CYGIMP_WALLCLOCK_HARDWARE 1
+
+#endif
