169dae167d5647bf69aef2596e8399f7ed7619dd
lepton.jjp <lepton.jjp@gmail.com>
2011-11-16 15:54:43 +0100

    add HAL for TWR-K60N512

---
+++ sys/user/tauon_sampleapp/hal/board_freescale_twrk60n512/lib/install/include/pkgconf/devs_eth_freescale_enet.h
@@ -0,0 +1,12 @@
+#ifndef CYGONCE_PKGCONF_DEVS_ETH_FREESCALE_ENET_H
+#define CYGONCE_PKGCONF_DEVS_ETH_FREESCALE_ENET_H
+/*
+ * File <pkgconf/devs_eth_freescale_enet.h>
+ *
+ * This file is generated automatically by the configuration
+ * system. It should not be edited. Any changes to this file
+ * may be overwritten.
+ */
+
+
+#endif
