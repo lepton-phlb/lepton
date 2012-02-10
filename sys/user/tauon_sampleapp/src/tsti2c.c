601305b5bb9fcb5f7fbe8346f4f6936b4dd4a751
lepton.jjp <lepton.jjp@gmail.com>
2012-02-10 10:28:27 +0100

    add new i2c devices

--- sys/user/tauon_sampleapp/src/tsti2c.c
+++ sys/user/tauon_sampleapp/src/tsti2c.c
@@ -50,8 +50,13 @@ either the MPL or the [eCos GPL] License."
 /*============================================
 | Global Declaration
 ==============================================*/
+#define MMA7660
+//#define M24xx
+
+#ifdef MMA7660
 //see KINETIS512_SC/src/projects/i2c from freescale website
 static signed char i2c_MMA7660_convert(signed char input);
+static int i2c_MMA7660_main(void);
 #define MMA7660_XOUT    0x0
 #define MMA7660_YOUT    0x1
 #define MMA7660_ZOUT    0x2
@@ -63,7 +68,16 @@ static signed char i2c_MMA7660_convert(signed char input);
 #define MMA7660_SR      0x8
 #define MMA7660_PDET    0x9
 #define MMA7660_PD      0xa
+#endif
 
+#ifdef M24xx
+
+#include "kernel/core/kernel.h"
+#define BUF_SIZE     9//24//32
+#define M24xx_SIZE   (32*1024)
+static int i2c_M24xx_main(void);
+
+#endif
 /*============================================
 | Implementation
 ==============================================*/
@@ -76,6 +90,79 @@ static signed char i2c_MMA7660_convert(signed char input);
 | See:
 ---------------------------------------------*/
 int tsti2c_main(int argc,char* argv[]) {
+   #ifdef MMA7660
+   i2c_MMA7660_main();
+   #endif
+   
+   #ifdef M24xx
+   i2c_M24xx_main();
+   #endif
+   
+   return 0;
+}
+
+#ifdef M24xx
+int i2c_M24xx_main(void) {
+   int fd=-1;
+   unsigned char buf_wr[BUF_SIZE];
+   unsigned char buf_rd[BUF_SIZE];
+   unsigned int eeprom_size=0;
+   
+   int r,cb_r,w,cb_w;
+   
+   int i, j;
+   int count = M24xx_SIZE/BUF_SIZE;
+   char c='a';
+   
+   if((fd = open("/dev/hd/hdb", O_RDWR, 0)) < 0) {
+      fprintf(stderr, "(EE) can't open /dev/hd/hdb\r\n");
+      return -1;
+   }
+   
+   lseek(fd, 0, SEEK_SET);
+   for(j=0; j<(count -1); j++) {
+      r=cb_r=w=cb_w=0;
+      memset((void*)buf_wr, c, BUF_SIZE);
+      
+      //
+      while((cb_w+=w) < BUF_SIZE){
+         if((w=write(fd, buf_wr+cb_w, BUF_SIZE-cb_w))<=0) {
+            fprintf(stderr, "close cb_w : %d\r\n", cb_w);
+            return -1;
+         }
+      }
+      
+      printf("[%d]", lseek(fd, -BUF_SIZE, SEEK_CUR));
+      usleep(100000);
+      
+      memset((void*)buf_rd, 0x00, BUF_SIZE);
+      while((cb_r+=r) < BUF_SIZE){
+         if((r=read(fd, buf_rd+cb_r, BUF_SIZE-cb_r))<=0) {
+            fprintf(stderr, "close cb_r : %d\r\n", cb_r);
+            close(fd);
+            return -1;
+         }
+      }
+      
+      for(i=0; i<BUF_SIZE; i++) {
+         printf("%x", buf_rd[i]);
+      }
+      printf("\r\n");
+      
+      if(++c>'z') {
+         c='a';
+      }
+      
+      usleep(100000);
+   }
+   
+   close(fd);
+   return 0;
+}
+#endif
+
+#ifdef MMA7660
+int i2c_MMA7660_main(void) {
    int fd=-1;
    signed char buf[2] = {0, 0};
    signed char xout, yout, zout;
@@ -83,7 +170,7 @@ int tsti2c_main(int argc,char* argv[]) {
    //note we name it i2c1 because i2c0 device name is already use
    //BUT it's really i2c0 (PTD8, PTD9)
    if((fd = open("/dev/i2c1", O_RDWR | O_NONBLOCK, 0)) < 0) {
-      fprintf(stderr, "can't open dev/i2c1\r\n");
+      fprintf(stderr, "(EE) can't open dev/i2c1\r\n");
       return -1;
    }
    
@@ -137,7 +224,7 @@ signed char i2c_MMA7660_convert(signed char input) {
    
    return (signed char) input;
 }
-
+#endif
 /*============================================
 | End of Source  : tsti2c.c
 |---------------------------------------------
