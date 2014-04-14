6652cf5af76c8baf86c5ae900c70f41a3d49f97c
lepton.phlb <lepton.phlb@gmail.com>
2014-04-14 17:53:38 +0200

    stm32f1 device driver.

--- sys/root/src/kernel/dev/arch/cortexm/stm32f1xx/i2c.c
+++ sys/root/src/kernel/dev/arch/cortexm/stm32f1xx/i2c.c
@@ -1,10 +1,30 @@
-/******************** (C) COPYRIGHT 2013 IJINUS ********************************
-* File Name          : i2c.c
-* Author             : Yoann TREGUIER
-* Version            : 1.2.0
-* Date               : 2013/06/14
-* Description        : I2C driver functions
-*******************************************************************************/
+/*
+The contents of this file are subject to the Mozilla Public License Version 1.1
+(the "License"); you may not use this file except in compliance with the License.
+You may obtain a copy of the License at http://www.mozilla.org/MPL/
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
+specific language governing rights and limitations under the License.
+
+The Original Code is Lepton.
+
+The Initial Developer of the Original Code is Philippe Le Boulanger.
+Portions created by Philippe Le Boulanger are Copyright (C) 2014 <lepton.phlb@gmail.com>.
+All Rights Reserved.
+
+Contributor(s): Jean-Jacques Pitrolle <lepton.jjp@gmail.com>.
+
+Alternatively, the contents of this file may be used under the terms of the eCos GPL license
+(the  [eCos GPL] License), in which case the provisions of [eCos GPL] License are applicable
+instead of those above. If you wish to allow use of your version of this file only under the
+terms of the [eCos GPL] License and not to allow others to use your version of this file under
+the MPL, indicate your decision by deleting  the provisions above and replace
+them with the notice and other provisions required by the [eCos GPL] License.
+If you do not delete the provisions above, a recipient may use your version of this file under
+either the MPL or the [eCos GPL] License."
+*/
+
 /* Includes ------------------------------------------------------------------*/
 #include "kernel/core/ucore/cmsis/Device/st/stm32f1xx/stm32f10x.h"
 #include "target.h"
@@ -279,5 +299,3 @@ int i2c_receive(u8 Addr, char *Ptr, u16 Size)
   while (I2C_GetFlagStatus(I2C1, I2C_FLAG_BUSY));
   return(ret);
 }
-
-/******************* (C) COPYRIGHT 2013 IJINUS ****************END OF FILE*****/
