2fed83b61a511443fe02a89dd8080a7f710524db
lepton.phlb <lepton.phlb@gmail.com>
2014-04-14 17:54:14 +0200

    stm32f4 device driver.

--- sys/root/src/kernel/dev/arch/cortexm/stm32f4xx/eth.h
+++ sys/root/src/kernel/dev/arch/cortexm/stm32f4xx/eth.h
@@ -1,13 +1,30 @@
-/*************************************************************************
- *
- *    Used with ARM IAR C/C++ Compiler
- *
- *    (c) Copyright IAR Systems 2009
- *
- *    $Revision: #1 $
- **************************************************************************/
+/*
+The contents of this file are subject to the Mozilla Public License Version 1.1
+(the "License"); you may not use this file except in compliance with the License.
+You may obtain a copy of the License at http://www.mozilla.org/MPL/
 
-//#include "my_includes.h"
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
+specific language governing rights and limitations under the License.
+
+The Original Code is Lepton.
+
+The Initial Developer of the Original Code is Philippe Le Boulanger.
+Portions created by Philippe Le Boulanger are Copyright (C) 2014 <lepton.phlb@gmail.com>.
+All Rights Reserved.
+
+Contributor(s): Jean-Jacques Pitrolle <lepton.jjp@gmail.com>.
+
+Alternatively, the contents of this file may be used under the terms of the eCos GPL license
+(the  [eCos GPL] License), in which case the provisions of [eCos GPL] License are applicable
+instead of those above. If you wish to allow use of your version of this file only under the
+terms of the [eCos GPL] License and not to allow others to use your version of this file under
+the MPL, indicate your decision by deleting  the provisions above and replace
+them with the notice and other provisions required by the [eCos GPL] License.
+If you do not delete the provisions above, a recipient may use your version of this file under
+either the MPL or the [eCos GPL] License."
+*/
+
 
 #ifndef  __ETHERNET_H
 #define  __ETHERNET_H
