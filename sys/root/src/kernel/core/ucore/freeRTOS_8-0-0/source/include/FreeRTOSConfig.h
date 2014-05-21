1bf39b076f74d7c3247f999847866c568cf9bf78
lepton.phlb <lepton.phlb@gmail.com>
2014-05-21 14:53:23 +0200

    added time slicing config.

--- sys/root/src/kernel/core/ucore/freeRTOS_8-0-0/source/include/FreeRTOSConfig.h
+++ sys/root/src/kernel/core/ucore/freeRTOS_8-0-0/source/include/FreeRTOSConfig.h
@@ -86,6 +86,7 @@
 #endif
 
 #define configUSE_PREEMPTION			1
+#define configUSE_TIME_SLICING      1
 #define configUSE_IDLE_HOOK				1
 #define configUSE_TICK_HOOK				1
 #define configCPU_CLOCK_HZ				( SystemCoreClock )
