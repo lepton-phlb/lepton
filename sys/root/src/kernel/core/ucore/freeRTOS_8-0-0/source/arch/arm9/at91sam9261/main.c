1fd24c6c8bf5162cf598296053ecbd315016512f
lepton.phlb <lepton.phlb@gmail.com>
2014-08-05 11:13:38 +0200

    add arch (cortex M0+, cortemx M4, arm926ejs) specific files for FreeRTOS 8 configuration.

---
+++ sys/root/src/kernel/core/ucore/freeRTOS_8-0-0/source/arch/arm9/at91sam9261/main.c
@@ -0,0 +1,121 @@
+/*
+    FreeRTOS V8.0.0:rc2 - Copyright (C) 2014 Real Time Engineers Ltd. 
+    All rights reserved
+
+    VISIT http://www.FreeRTOS.org TO ENSURE YOU ARE USING THE LATEST VERSION.
+
+    ***************************************************************************
+     *                                                                       *
+     *    FreeRTOS provides completely free yet professionally developed,    *
+     *    robust, strictly quality controlled, supported, and cross          *
+     *    platform software that has become a de facto standard.             *
+     *                                                                       *
+     *    Help yourself get started quickly and support the FreeRTOS         *
+     *    project by purchasing a FreeRTOS tutorial book, reference          *
+     *    manual, or both from: http://www.FreeRTOS.org/Documentation        *
+     *                                                                       *
+     *    Thank you!                                                         *
+     *                                                                       *
+    ***************************************************************************
+
+    This file is part of the FreeRTOS distribution.
+
+    FreeRTOS is free software; you can redistribute it and/or modify it under
+    the terms of the GNU General Public License (version 2) as published by the
+    Free Software Foundation >>!AND MODIFIED BY!<< the FreeRTOS exception.
+
+    >>! NOTE: The modification to the GPL is included to allow you to distribute
+    >>! a combined work that includes FreeRTOS without being obliged to provide
+    >>! the source code for proprietary components outside of the FreeRTOS
+    >>! kernel.
+
+    FreeRTOS is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+    FOR A PARTICULAR PURPOSE.  Full license text is available from the following
+    link: http://www.freertos.org/a00114.html
+
+    1 tab == 4 spaces!
+
+    ***************************************************************************
+     *                                                                       *
+     *    Having a problem?  Start by reading the FAQ "My application does   *
+     *    not run, what could be wrong?"                                     *
+     *                                                                       *
+     *    http://www.FreeRTOS.org/FAQHelp.html                               *
+     *                                                                       *
+    ***************************************************************************
+
+    http://www.FreeRTOS.org - Documentation, books, training, latest versions,
+    license and Real Time Engineers Ltd. contact details.
+
+    http://www.FreeRTOS.org/plus - A selection of FreeRTOS ecosystem products,
+    including FreeRTOS+Trace - an indispensable productivity tool, a DOS
+    compatible FAT file system, and our tiny thread aware UDP/IP stack.
+
+    http://www.OpenRTOS.com - Real Time Engineers ltd license FreeRTOS to High
+    Integrity Systems to sell under the OpenRTOS brand.  Low cost OpenRTOS
+    licenses offer ticketed support, indemnification and middleware.
+
+    http://www.SafeRTOS.com - High Integrity Systems also provide a safety
+    engineered and independently SIL3 certified version for use in safety and
+    mission critical applications that require provable dependability.
+
+    1 tab == 4 spaces!
+*/
+
+/*
+ * Creates all the demo application tasks, then starts the scheduler.  The WEB
+ * documentation provides more details of the standard demo application tasks.
+ *
+ * A "Check" task is created in addition to the standard demo tasks.    This
+ * only executes every three seconds but has a high priority to ensure it gets
+ * processor time.  Its main function is to check that all the standard demo
+ * tasks are still operational.  If everything is running as expected then the
+ * check task will toggle an LED every 3 seconds.  An error being discovered in
+ * any task will cause the toggle rate to increase to 500ms.
+ *
+ */
+
+/* FreeRTOS includes. */
+#include "kernel/core/kernel.h"
+
+/* Atmel library includes. */
+#include <pio/pio.h>
+
+/*-----------------------------------------------------------*/
+
+/* Simple hardware setup required by the demo. */
+static void prvSetupHardware( void );
+
+/*-----------------------------------------------------------*/
+int main(void)
+{
+	/* Configure the hardware ready to run the test. */
+	prvSetupHardware();
+	
+   //lepton kernel start 
+   _start_kernel(0);
+
+	/* The following function will only create more tasks and timers if
+	mainCREATE_SIMPLE_LED_FLASHER_DEMO_ONLY is set to 0 (at the top of this
+	file).  See the comments at the top of this file for more information. */
+	//prvOptionallyCreateComprehensveTestApplication();
+
+	/* Start the scheduler. */
+	vTaskStartScheduler();
+	
+	/* If all is well, the scheduler will now be running, and the following line
+	will never be reached.  If the following line does execute, then there was
+	insufficient FreeRTOS heap memory available for the idle and/or timer tasks
+	to be created.  See the memory management section on the FreeRTOS web site
+	for more details. */
+	for( ;; );	
+}
+/*-----------------------------------------------------------*/
+
+
+static void prvSetupHardware( void )
+{
+  return;
+}
+
