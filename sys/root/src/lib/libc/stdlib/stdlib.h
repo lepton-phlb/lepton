d6bc21238cbf5e3ed504550bb990d7800957bf44
Jean-Jacques Pitrolle <lepton.jjp@gmail.com>
2014-08-12 13:59:42 +0200

    Add missing div and ldiv functions in custom stdlib directory

---
+++ sys/root/src/lib/libc/stdlib/stdlib.h
@@ -0,0 +1,26 @@
+/*
+ * Add appropriate license
+ */
+#ifndef __CUSTOM_STDLIB_H__
+#define __CUSTOM_STDLIB_H__
+
+#include <stddef.h>
+
+typedef struct {
+    int quot;
+    int rem;
+} div_t;
+
+typedef struct {
+    long quot;
+    long rem;
+} ldiv_t;
+
+
+div_t div(int num, int den);
+/* __attribute__((__const__));*/
+
+ldiv_t ldiv(long num, long den);
+/*__attribute__((__const__));*/
+
+#endif /*__CUSTOM_STDLIB_H__*/
