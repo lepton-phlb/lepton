d679c2514e9eba8fd578b995a4746652a3b1a7b5
Jean-Jacques Pitrolle <lepton.jjp@gmail.com>
2015-01-06 17:04:36 +0100

    Increase stack size to 20Kio for samd20

--- sys/root/src/kernel/core/ucore/freeRTOS_8-0-0/source/arch/cortex-m0+/at91samd20/FreeRTOSConfig.h
+++ sys/root/src/kernel/core/ucore/freeRTOS_8-0-0/source/arch/cortex-m0+/at91samd20/FreeRTOSConfig.h
@@ -88,7 +88,7 @@
 #define configTICK_RATE_HZ				( ( portTickType ) 1000 )
 #define configMAX_PRIORITIES			( ( unsigned portBASE_TYPE ) 8 )
 #define configMINIMAL_STACK_SIZE		( ( unsigned short ) 70 )
-#define configTOTAL_HEAP_SIZE			( ( size_t ) ( 16*1024 ) )
+#define configTOTAL_HEAP_SIZE			( ( size_t ) ( 20*1024 ) )
 #define configMAX_TASK_NAME_LEN			( 5 )
 #define configUSE_TRACE_FACILITY		1
 #define configUSE_16_BIT_TICKS			0
